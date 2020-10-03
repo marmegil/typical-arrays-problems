da9cb36cbf550af896e822e764ad2a1f729c7933
marmegil <kirill.german50@gmail.com>
2020-10-03 19:19:35 +0300

    All test complite

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  const min = Math.min.apply(null, array);
+  if (min == Infinity) return 0;
+  else return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  const max = Math.max.apply(null, array);
+  if (max == -Infinity) return 0;
+  else return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  else if (array.length == 0) return 0;
+  else return array.reduce((a, b) => (a + b)) / array.length;;
 }
